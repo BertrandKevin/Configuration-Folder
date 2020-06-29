c287fa56e3b1f0e13958a605f60a6b7bd4981956
Kevin Bertrand <41152945+BertrandKevin@users.noreply.github.com>
2020-06-29 09:50:25 +0200

    Update configuration.sh

--- configuration.sh
+++ configuration.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-expect -c 'spawn su - ssn -c "bash /home/dev/Configuration-Folder/installationAndConfiguration.sh"; expect "Password:"; send "Xf@9=qZc\n"; interact'
+expect -c 'spawn su - ssn -c "python /home/dev/Configuration-Folder/src/app.py"; expect "Password:"; send "Xf@9=qZc\n"; interact'
 echo "L'installation est terminée, le routeur va redémarrer automatiquement dans 1 minute!"
