7b61bb8461b472bc94e2e24a09fcbfb45b9c5a46
Kevin Bertrand <41152945+BertrandKevin@users.noreply.github.com>
2020-07-02 14:35:58 +0200

    Delete configuration.sh

--- configuration.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-expect -c 'spawn su - ssn -c "python /home/dev/Configuration-Folder/src/app.py"; expect "Password:"; send "Xf@9=qZc\n"; interact'
-echo "L'installation est terminée, le routeur va redémarrer automatiquement dans 1 minute!"
